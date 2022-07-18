fec9edc204a5462f8596499501d7d6c3b4243cfa
vinsdragonis <zrexteam128@gmail.com>
2022-07-18 21:32:30 +0530

    Restructured binaries

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-nasm -f bin -o boot.bin boot.asm
-nasm -f bin -o loader.bin ./loader/loader.asm
+nasm -f bin -o ./bin/boot.bin boot.asm
+nasm -f bin -o ./bin/loader.bin ./loader/loader.asm
 nasm -f elf64 -o kernel.o kernel.asm
 nasm -f elf64 -o trapa.o trap.asm
 nasm -f elf64 -o liba.o ./lib/lib.asm
@@ -7,7 +7,7 @@ gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone -c
 gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone -c trap.c 
 gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone -c print.c 
 ld -nostdlib -T link.lds -o kernel kernel.o main.o trapa.o trap.o liba.o print.o
-objcopy -O binary kernel kernel.bin 
-dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
-dd if=loader.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
-dd if=kernel.bin of=boot.img bs=512 count=100 seek=6 conv=notrunc
+objcopy -O binary kernel ./bin/kernel.bin 
+dd if=./bin/boot.bin of=boot.img bs=512 count=1 conv=notrunc
+dd if=./bin/loader.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
+dd if=./bin/kernel.bin of=boot.img bs=512 count=100 seek=6 conv=notrunc
