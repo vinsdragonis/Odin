1c77cd974ee744010d19de10d53c9c7d5943c2f7
vinsdragonis <zrexteam128@gmail.com>
2022-07-27 03:15:03 +0530

    .

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include "./trap/trap.h"
 #include "print/print.h"
 #include "./memory/memory.h"
+#include "./process/process.h"
 
 void KMain(void)
 {
    init_idt();
    init_memory();
    init_kvm();
-   show_total_memory();
+   init_process();
+   launch();
+   // show_total_memory();
 }
