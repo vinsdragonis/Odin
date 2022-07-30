47ba8801f45ee58e629b0cf857b1ecc98697c895
vinsdragonis <zrexteam128@gmail.com>
2022-07-31 02:57:02 +0530

    Removed redundant files

--- usr/build.sh
+++
@@ -1,4 +0,0 @@
-nasm -f elf64 -o ./obj/start.o start.asm
-gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone -c main.c -o ./obj/main.o
-ld -nostdlib -Tlink.lds -o usr ./obj/start.o ./obj/main.o ../lib/lib.a
-objcopy -O binary usr ./bin/usr.bin
--- usr/lib.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LIB_H_
-#define _LIB_H_
-
-int printf(const char *format, ...);
-
-#endif
